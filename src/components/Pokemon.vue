fcbdd49f9c59f48382e0deb6531b1e1c893f9b71
Elena7391 <lopezslabaelena@gmail.com>
2024-07-07 19:17:20 +0200

    first changes

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,21 @@
+<script>
+    let numPokemon=Math.random()*1008 + 1;
+    let pokemon="https://pokeapi.co/api/v2/pokemon/"+numPokemon;
+    fetch(pokemon)
+    .then(res=>res.json());
+    let pokname=res.name();
+    let image=res.sprite;
+    
+    document.getElementById("button").onclick= function (){
+        numPokemon=Math.random()*1008 + 1;
+    }
+
+    let h1=document.createElement("h1");
+    h1.innerHTML=pokname;
+    document.appendChild(h1);
+
+    let pokimg=document.createElement("img");
+    
+</script>
+
+<button id="button">Boton</button>
